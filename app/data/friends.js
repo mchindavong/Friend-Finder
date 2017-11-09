03d52e3334efd4ad439be9f687d72645e0f43b15
Mollie <molliechindavong@gmail.com>
2017-11-09 15:06:02 -0500

    updating

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+        name: "Monica Gellar",
+        picture: "https://media.giphy.com/media/IdyHPMy8eqZ68/giphy.gif",
+        scores: [
+            "3",
+            "5",
+            "1",
+            "4",
+            "1",
+            "3",
+            "5",
+            "2",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: "Rachel Green",
+        picture: "https://media.giphy.com/media/w5M9QgelugIJG/giphy.gif",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "5",
+            "2",
+            "1",
+            "3",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: "Phoebe Buffay",
+        picture: "https://media.giphy.com/media/EVNHAa5P68F1u/giphy.gif",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Ross Gellar",
+        picture: "https://thumbs.gfycat.com/AppropriateEdibleGoldenretriever-max-1mb.gif",
+        scores: [
+            "3",
+            "4",
+            "2",
+            "2",
+            "5",
+            "3",
+            "4",
+            "2",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Chandler Bing",
+        picture: "https://media.giphy.com/media/nV7gQOxmt2Ita/giphy.gif",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Joey Tribbiani",
+        picture: "https://media.giphy.com/media/11SIBu3s72Co8w/giphy.gif",
+        scores: [
+            "2",
+            "1",
+            "5",
+            "3",
+            "4",
+            "2",
+            "3",
+            "5",
+            "1",
+            "2"
+        ]
+    },
+
+];
+
+module.exports = friends;
